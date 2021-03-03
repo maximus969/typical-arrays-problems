0aa59ef6439d240fc9b95a1fbe66ed4a88c46ca7
Maxim Bordea <maxbordea@gmail.com>
2021-03-03 22:31:20 +0200

    fix: implement the logic of min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+const isArrayValid = (array) => !(typeof array === 'undefined' || array.length === 0)
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!isArrayValid(array)) {
+    return 0;
+  }
+  const minOfArray = (acc, current) => Math.min(acc, current);
+  return array.reduce(minOfArray, array[0]);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+  if (!isArrayValid(array)) {
+    return 0;
+  }
+  const maxOfArray = (acc, current) => Math.max(acc, current);
+  return array.reduce(maxOfArray, array[0]);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+  if (!isArrayValid(array)) {
+    return 0;
+  }
+  const sumOfArray = (acc, current) => acc + current;
+  return array.reduce(sumOfArray, 0) / array.length;
 }
